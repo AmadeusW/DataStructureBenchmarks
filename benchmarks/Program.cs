3b828a4571a7c0d6264bc0d098bc9ddcf077e0fa
Amadeusz Wieczorek <amwieczo@microsoft.com>
2018-12-19 10:13:59 -0800

    sets up solution and project

---
+++ benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Deo.DataStructureBenchmarks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
